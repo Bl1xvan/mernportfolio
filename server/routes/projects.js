6f37d342c4b37d88d5b0c96113014f73b45d2411
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-11-16 13:13:52 -0500

    first commit

---
+++ server/routes/projects.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const {getAllProjects} = require('../controllers/projects')
+
+router.route('/').get(getAllProjects)
+module.exports = router;
