ae96010d50d2caad87fbb9f181144d1aefed569b
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-11-21 18:15:50 -0500

    dirname

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5000;
 
 
 
-app.use('/', express.static(join(__dir__, 'client', 'build')));
+app.use('/', express.static(join(__dirname, 'client', 'build')));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
