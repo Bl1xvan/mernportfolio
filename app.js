b02e7871efbad27e2fa2687206a5e583bca41b73
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-11-23 09:20:42 -0500

    added authroutes

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require("cors");
+const authRoutes = require('./routes/auth-routes');
 const projects = require('./routes/projects');
 const connectDB = require('./db/connect');
 const { join } = require("path");
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(cors)
+
 app.use('/', express.static('./public'));
 
 app.use(express.json())
 app.use('/api/v1/projects', projects)
+app.use('/api', authRoutes);
 
 app.use(notFound)
 app.use(errorHandlerMiddleware)
