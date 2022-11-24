307b8d5f731c443b24c15451aa0d654ddfb0830c
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-11-24 14:06:02 -0500

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-
+const cors = require('cors');
+const morgan = require('morgan')
 
 
 const projects = require('./routes/projects');
@@ -10,8 +11,9 @@ const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const app = express();
+app.use(morgan('dev'))
+app.use(cors({origin: true, credentials: true}))
 app.use('/', express.static('./public'));
-
 app.use(express.json())
 app.use('/api/v1/projects', projects)
 
