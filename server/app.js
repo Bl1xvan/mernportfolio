ed9ed1b73ba33f8a337b22f0b557a57682f72c30
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-11-23 19:45:07 -0500

    added cors and morgan

---
+++ server/app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const authRoutes = require('./routes/auth-routes');
+const projects = require('./routes/projects');
+const connectDB = require('./db/connect');
+const { join } = require("path");
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use('/', express.static('./public'));
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors({origin: true, credentials: true}))
+app.use('/api/v1/projects', projects)
+app.use('/api', authRoutes);
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000;
+
+
+
+app.use('/', express.static(join('client', 'build')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join("client", "build", "index.html"));
+  });
+
+
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    }catch(error){
+        console.log(error)
+    }
+}
+
+start()
