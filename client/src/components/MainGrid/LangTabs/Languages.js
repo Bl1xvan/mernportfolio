62c83deacf86b8f64d040c5470065ecb588104d4
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-12-05 20:13:21 -0500

    more styling

--- client/src/components/MainGrid/LangTabs/Languages.js
+++ client/src/components/MainGrid/LangTabs/Languages.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {v4} from 'uuid'
 
 const Languages = ({languages}) => {
-    return (<div className="flex-row">{languages.map(l => (<div key={v4()} className="lang">{l}</div>))}</div>)
+    return (<div className="flex-row">{languages.map(l => (<div key={v4()} className="colored rounded">{l}</div>))}</div>)
 }
 
 export default Languages
--- client/src/components/MainGrid/Projects/Overlay.js
+++ client/src/components/MainGrid/Projects/Overlay.js
@@ -7,15 +7,15 @@ export default function Overlay({project, displayChange, displayNone}) {
         <div style={displayChange} className="fixed full-container overlay-all">
           <div className="position-center rounded">
                 <button type="button" onClick={displayNone}>X</button>
-                <div className="grid-group medium-grid">
+                <div className="grid-group medium-grid scroll">
                   <div className="centered"><img src={image} alt={title} className="full-container" /></div>
                   <div className="grid-group align-row">
                       <h2>{title}</h2>
                       <Languages languages={languages} />
                       <p className="description">{description}</p>
                       <div className="bothends">
-                        <a className="rounded" href={repository}>Repository</a>
-                        <a className="rounded" href={deployment}>Deployment</a>
+                        <a className="colored rounded" href={repository}>Repository</a>
+                        <a className="colored rounded" href={deployment}>Deployment</a>
                       </div>
                   </div>
                 </div>
