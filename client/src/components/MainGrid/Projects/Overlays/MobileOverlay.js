8037d568de34e78824e073716cae0a61902179f5
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-12-08 13:56:49 -0500

    changing overlay

--- client/src/components/MainGrid/Projects/Overlays/MobileOverlay.js
+++ client/src/components/MainGrid/Projects/Overlays/MobileOverlay.js
@@ -13,10 +13,12 @@ export default function MobileOverlay({project, displayNone}) {
                       <h2>{title}</h2>
                       <Languages languages={languages} />
                       <p className="description">{description}</p>
-                      <div className="bothends">
-                        <a className="rounded purplediv" href={repository}>Repository</a>
-                        <a className="rounded purplediv" href={deployment}>Deployment</a>
-                      </div>
+                      <a className="rounded purplediv" href={repository}>
+                        Repository
+                      </a>
+                      <a className="rounded purplediv" href={deployment}>
+                        Deployment
+                      </a>
                   </div>
               </div>
         </div>
