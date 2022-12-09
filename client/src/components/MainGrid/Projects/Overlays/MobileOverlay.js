ad3d26ebf01830bcccff861e795c7a090489b5f1
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-12-08 20:44:47 -0500

    still working on it

--- client/src/components/MainGrid/Projects/Overlays/MobileOverlay.js
+++ client/src/components/MainGrid/Projects/Overlays/MobileOverlay.js
@@ -5,7 +5,7 @@ export default function MobileOverlay({project, displayNone}) {
     const {image, title, repository, deployment, description, languages} = project
     const separated = {marginTop: "15px", marginRight: "15px", alignSelf: "center", maxWidth: "225px" }
     return (
-          <div className="absolute overlay-all transdarkgray">
+          <div className="absolute full-container overlay-all transdarkgray">
             <div className="position-center rounded gray-section label-flex">
                 <button type="button" onClick={displayNone}>X</button>
                   <img src={image} alt={title} style={{width: "95%", height: "auto", margin: "0 auto", maxWidth: "350px"}}/>
